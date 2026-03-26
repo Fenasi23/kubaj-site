4da90f45e6c0d5ece029458ef0a49be62a45af6a
kubaj <haticebeyzabilici@hotmail.com>
2026-03-26 14:59:42 +0300

    Final verification fix

--- backend/loader.js
+++
@@ -1,8 +0,0 @@
-console.log('Loader started');
-try {
-    console.log('Attempting to require server.js...');
-    require('./server.js');
-    console.log('Server.js required successfully.');
-} catch (e) {
-    console.error('Loader caught error:', e);
-}
